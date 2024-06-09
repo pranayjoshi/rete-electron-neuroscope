a130107ce5f4dd09b28efc19218d9f40c2b97806
pranayjoshi <pranayjoshi4466@gmail.com>
2024-06-09 14:09:46 -0500

    updated initialization

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true, // Ensure context isolation is enabled for security
+            enableRemoteModule: true 
+    },
+  });
 
-    mainWindow.loadFile('index.html');
+  mainWindow.loadFile('src/index.html');
+  mainWindow.webContents.openDevTools()
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow();
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });

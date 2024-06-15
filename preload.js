d23ee27a91b672b77fa7fa2b0163145e458a8d61
pranayjoshi <pranayjoshi4466@gmail.com>
2024-06-15 09:52:17 -0500

    updated

--- preload.js
+++ preload.js
@@ -1,10 +1,14 @@
 const { contextBridge } = require('electron');
 const Rete = require('rete');
-const ConnectionPlugin = require('rete-connection-plugin').default;
-const VueRenderPlugin = require('rete-vue-render-plugin').default;
+const ConnectionPlugin = require('rete-connection-plugin');
+const VueRenderPlugin = require('rete-vue-render-plugin');
+
+console.log('Rete:', Rete); // Logging to verify import
+console.log('ConnectionPlugin:', ConnectionPlugin); // Logging to verify import
+console.log('VueRenderPlugin:', VueRenderPlugin); // Logging to verify import
 
 contextBridge.exposeInMainWorld('api', {
-    Rete,
-    ConnectionPlugin,
-    VueRenderPlugin
+    Rete: Rete.default || Rete,
+    ConnectionPlugin: ConnectionPlugin.default || ConnectionPlugin,
+    VueRenderPlugin: VueRenderPlugin.default || VueRenderPlugin
 });
--- rete.js
+++ rete.js
@@ -28,24 +28,51 @@
 //     editor.view.resize();
 //     editor.trigger('process');
 // })();
+// const { Rete, ConnectionPlugin, VueRenderPlugin } = window.api;
 
-const { Rete, ConnectionPlugin, VueRenderPlugin } = window.api;
+// console.log('Rete:', Rete); // Logging to verify import
+// console.log('ConnectionPlugin:', ConnectionPlugin); // Logging to verify import
+// console.log('VueRenderPlugin:', VueRenderPlugin); // Logging to verify import
+
+// (async () => {
+//     const container = document.querySelector('#rete');
+//     const { NodeEditor } = Rete;
+    
+//     console.log(NodeEditor)
+//     const editor = new NodeEditor('demo@0.1.0', container);
+//     editor.use(ConnectionPlugin);
+//     editor.use(VueRenderPlugin);
+
+//     const engine = new Rete.Engine('demo@0.1.0');
+
+//     editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
+//         console.log('process');
+//         await engine.abort();
+//         await engine.process(editor.toJSON());
+//     });
+
+//     editor.view.resize();
+//     editor.trigger('process');
+// })();
+
+const Rete = require('rete');
+const ConnectionPlugin = require('rete-connection-plugin');
+const VueRenderPlugin = require('rete-vue-render-plugin');
 
 (async () => {
-    const container = document.querySelector('#rete');
+  const container = document.querySelector('#rete');
+  const editor = new Rete.NodeEditor('demo@0.1.0', container);
+  editor.use(ConnectionPlugin);
+  editor.use(VueRenderPlugin);
 
-    const editor = new Rete.NodeEditor('demo@0.1.0', container);
-    editor.use(ConnectionPlugin);
-    editor.use(VueRenderPlugin);
+  const engine = new Rete.Engine('demo@0.1.0');
 
-    const engine = new Rete.Engine('demo@0.1.0');
+  editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
+    console.log('process');
+    await engine.abort();
+    await engine.process(editor.toJSON());
+  });
 
-    editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
-        console.log('process');
-        await engine.abort();
-        await engine.process(editor.toJSON());
-    });
-
-    editor.view.resize();
-    editor.trigger('process');
+  editor.view.resize();
+  editor.trigger('process');
 })();

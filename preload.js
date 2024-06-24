26f2d78a253e088c763c4d2b8e44087c157d6452
pranayjoshi <pranayjoshi4466@gmail.com>
2024-06-23 23:28:50 -0500

    updated specific files

--- preload.js
+++ preload.js
@@ -1,1 +1,10 @@
-window.rete = require('./rete.js');
+const { contextBridge } = require('electron');
+const Rete = require('rete');
+const ConnectionPlugin = require('rete-connection-plugin').default;
+const VueRenderPlugin = require('rete-vue-render-plugin').default;
+
+contextBridge.exposeInMainWorld('api', {
+  Rete: Rete.default || Rete,
+  ConnectionPlugin,
+  VueRenderPlugin
+});
